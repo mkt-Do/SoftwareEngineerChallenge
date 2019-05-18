ae254e14d4d418a1e54fca98744374c84098b50c
mkt-Do <mkt.koro.mikuru@gmail.com>
2019-05-18 11:22:40 +0900

    modified stack over flow error

--- queue/src/main/scala/com/github/mktdo/queue/Queue.scala
+++ queue/src/main/scala/com/github/mktdo/queue/Queue.scala
@@ -7,19 +7,42 @@ trait Queue[T] {
   def head: Option[T]
 }
 
-class NonEmptyQueue[T](val first: T, val last: T, val rest: Queue[T]) extends Queue[T] {
+class NonEmptyQueue[T](val first: T, val rest: Queue[T]) extends Queue[T] {
   def isEmpty: Boolean = false
-  def enQueue(t: T): Queue[T] = new NonEmptyQueue[T](first, t, rest.enQueue(t))
+  def enQueue(t: T): Queue[T] = {
+    sealed trait TailRec[A] {
+      final def run: A = this match {
+        case Return(v) => v
+        case Suspend(k) => k().run
+      }
+    }
+    case class Return[A](v: A) extends TailRec[A]
+    case class Suspend[A](k: () => TailRec[A]) extends TailRec[A]
+
+    def loop(qs: Queue[T], k: Queue[T] => TailRec[Queue[T]]): TailRec[Queue[T]] = qs match {
+      case e: EmptyQueue[T] => k(new NonEmptyQueue[T](t, e))
+      case NonEmptyQueue(f, r) => loop(r, q => Suspend(() => k(new NonEmptyQueue[T](f, q))))
+    }
+    val f: Queue[T] => TailRec[Queue[T]] = q => Return(q)
+    loop(this, f).run
+  }
   def deQueue(): Queue[T] = rest.head match {
-    case Some(el) => new NonEmptyQueue[T](el, last, rest.deQueue())
+    case Some(el) => rest
     case None => Queue.empty[T]
   }
   def head: Option[T] = Some(first)
 }
 
+object NonEmptyQueue {
+  def unapply[T](queue: Queue[T]): Option[(T, Queue[T])] = queue match {
+    case q: NonEmptyQueue[T] => Some((q.first, q.rest))
+    case _ => None
+  }
+}
+
 class EmptyQueue[T] extends Queue[T] {
   def isEmpty: Boolean = true
-  def enQueue(t: T): Queue[T] = new NonEmptyQueue[T](t, t, this)
+  def enQueue(t: T): Queue[T] = new NonEmptyQueue[T](t, this)
   def deQueue() = Queue.empty[T]
   def head: Option[T] = None
 }
